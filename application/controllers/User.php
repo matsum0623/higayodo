379fb4957f424121dc80f4b38eb3c97aca969a18
matsum0623 <matsum0623@gmail.com>
2017-03-10 09:42:50 +0000

    create user page

---
+++ application/controllers/User.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+    public function __construct(){
+    
+        parent::__construct();
+
+        $data['title'] = 'ユーザー専用 / 東淀川区 地域情報データベース';
+        $data['page'] = 'user';
+        $this->load->vars($data);
+    	if( ! $this->session->userdata("is_logged_in")){
+    	    redirect('login');
+    	}
+    }
+
+	public function index()
+	{
+		$this->load->view('user_view');
+	}
+}
